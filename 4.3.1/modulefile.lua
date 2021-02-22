77366c2e51abad1c2a11325b47c232e52a08a9bd
Ivan Cao-Berg <icaoberg@psc.edu>
2021-02-22 03:17:28 -0500

    Added modulefile

---
+++ 4.3.1/modulefile.lua
@@ -0,0 +1,47 @@
+--
+-- ffmpeg 4.3.1 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: A complete, cross-platform solution to record, convert and stream audio and video."
+-- "Keywords: singularity utilities"
+
+whatis("Name: ffmpeg")
+whatis("Version: 4.3.1")
+whatis("Category: Other")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: A complete, cross-platform solution to record, convert and stream audio and video.")
+
+help([[
+ffmpeg 4.3.1
+-------------
+
+Description
+-----------
+A complete, cross-platform solution to record, convert and stream audio and video.
+
+To load the module type
+
+> module load ffmpeg/4.3.1
+
+To unload the module type
+
+> module unload ffmpeg/4.3.1
+
+Documentation
+-------------
+https://ffmpeg.org/documentation.html
+
+Repository
+----------
+https://github.com/FFmpeg/FFmpeg
+
+Tools included in this module are
+
+* ffmpeg
+]])
+
+local package = "ffmpeg"
+local version = "4.3.1"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
