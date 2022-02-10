d9c2cbf69f14d79502fb1fa2126d4b8decde2963
Ivan Cao-Berg <icaoberg@psc.edu>
2022-02-10 14:06:48 -0500

    Added scripts for version 4.4.1-r3

---
+++ 4.4.1-r3/modulefile.lua
@@ -0,0 +1,41 @@
+--
+-- ffmpeg 4.4.1-r3 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: A complete, cross-platform solution to record, convert and stream audio and video."
+-- "Keywords: singularity utilities"
+
+whatis("Name: ffmpeg")
+whatis("Version: 4.4.1-r3")
+whatis("Category: Other")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: A complete, cross-platform solution to record, convert and stream audio and video.")
+
+help([[
+Description
+-----------
+A complete, cross-platform solution to record, convert and stream audio and video.
+
+To load the module type
+
+> module load ffmpeg/4.4.1-r3
+
+To unload the module type
+
+> module unload ffmpeg/4.4.1-r3
+
+Documentation
+-------------
+https://ffmpeg.org/documentation.html
+https://github.com/FFmpeg/FFmpeg
+
+Tools included in this module are
+
+* ffmpeg
+]])
+
+local package = "ffmpeg"
+local version = "4.4.1-r3"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
