1349367afde7f5c1788ecf9965b6d5184da9f7d3
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2022-04-05 16:42:29 -0400

    Updated 5.0-r1

---
+++ 5.0-r1/dbuild.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+docker build -t icaoberg/ffmpeg:5.0-r1 .
---
+++ 5.0-r1/modulefile.lua
@@ -0,0 +1,41 @@
+--
+-- ffmpeg 5.0-r1 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: A complete, cross-platform solution to record, convert and stream audio and video."
+-- "Keywords: singularity utilities"
+
+whatis("Name: ffmpeg")
+whatis("Version: 5.0-r1")
+whatis("Category: Other")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: A complete, cross-platform solution to record, convert and stream audio and video.")
+
+help([[
+Description
+-----------
+A complete, cross-platform solution to record, convert and stream audio and video.
+
+To load the module type
+
+> module load ffmpeg/5.0-r1
+
+To unload the module type
+
+> module unload ffmpeg/5.0-r1
+
+Documentation
+-------------
+https://ffmpeg.org/documentation.html
+https://github.com/FFmpeg/FFmpeg
+
+Tools included in this module are
+
+* ffmpeg
+]])
+
+local package = "ffmpeg"
+local version = "5.0-r1"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
